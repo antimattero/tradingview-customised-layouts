d9f89af13200551b3a66bb057d0dcf0f04f546b7
antimattero <antimattero3.14@gmail.com>
2020-05-12 00:49:55 +0100

    First commit

---
+++ includes/top-navigation.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>top navigation menu</title>
+<meta name="description" content="top navigation menu">
+<meta name="author" content="antimattero">
+
+<style type="text/css">
+
+.nav-content a{
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    text-decoration: none;
+    padding: 7px 16px;
+    font-size: 16px;
+}
+.nav-content a:hover {color: #c9c9c9;}
+
+</style>
+</head>
+
+<body>
+
+<nav class="nav-content">
+
+    <a href="#">Home</a>
+    <a href="#">Crypto</a>
+    <a href="#">Forex</a>
+    <a href="#">Commodities</a>
+    <a href="#">Shares</a>
+    <a href="#">Indicies</a>
+
+</nav>
+
+</body>
+</html>
